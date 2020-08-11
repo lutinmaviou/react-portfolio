3650247197fcfa035ff5133a2cc31f4a089ad835
Bertrand Bourion <bertrandbou@hotmail.fr>
2020-08-11 16:54:09 +0200

    feat: added CV view at about section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,43 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import StarIcon from '../../icons/StarIcon';
-import CV from '../../shared/img/CV.png';
+import Cv from './Cv';
 
-export default () => {
-    return (
-        <section className="page-section bg-primary text-white mb-0" id="about">
-            <div className="container">
+export default class About extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { showCv: false }
+    }
 
-                <h2 className="page-section-heading text-center text-uppercase text-white">À propos de moi</h2>
+    handleShowCv = () => {
+        this.setState({ showCv: true });
+    }
 
-                <div className="divider-custom divider-light">
-                    <div className="divider-custom-line"></div>
-                    <div className="divider-custom-icon">
-                        <StarIcon />				</div>
-                    <div className="divider-custom-line"></div>
-                </div>
+    handleCloseCv = () => {
+        this.setState({ showCv: false });
+    }
 
-                <div className="row">
-                    <div className="col-lg-4 ml-auto">
-                        <p className="lead">Ancien professionnel de la restauration, j'ai décidé en 2019 de me reconvertir dans
-                        la programmation de sites web et applications. Le titre de développeur web est désormais acquis
-                        grâce au parcours
-                        du même nom que j'ai suivi par correspondance avec l'école
+    render() {
+        return (
+            <>
+                <section className="page-section bg-primary text-white mb-0" id="about">
+                    <div className="container">
+
+                        <h2 className="page-section-heading text-center text-uppercase text-white">À propos de moi</h2>
+
+                        <div className="divider-custom divider-light">
+                            <div className="divider-custom-line"></div>
+                            <div className="divider-custom-icon"><StarIcon /></div>
+                            <div className="divider-custom-line"></div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-lg-4 ml-auto">
+                                <p className="lead">Ancien professionnel de la restauration, j'ai décidé en 2019 de me reconvertir dans
+                                la programmation de sites web et applications. Le titre de développeur web est désormais acquis
+                                grâce au parcours
+                                du même nom que j'ai suivi par correspondance avec l'école
 						<a href="https://openclassrooms.com/fr/paths/48-developpeur-web-junior" target="blank"
-                                id="ocr"> Openclassrooms</a>
-                        </p>
-                    </div>
-                    <div className="col-lg-4 mr-auto">
-                        <p className="lead">Ce site est destiné à en savoir un peu sur moi et découvrir mes réalisations.
-                        Mon temps étant désormais consacré à la création de divers projets, la section portfolio devrait
+                                        id="ocr"> Openclassrooms</a>
+                                </p>
+                            </div>
+                            <div className="col-lg-4 mr-auto">
+                                <p className="lead">Ce site est destiné à en savoir un peu sur moi et découvrir mes réalisations.
+                                Mon temps étant désormais consacré à la création de divers projets, la section portfolio devrait
 						s'alimenter régulièrement, car je suis définitivement corrompu par ce métier passionant !</p>
+                            </div>
+                        </div>
+                        <div className="text-center mt-4">
+                            <a onClick={this.handleShowCv} id="show-cv" className="btn btn-xl btn-outline-light rounded js-scroll-trigger" href="#myCv">
+                                Voir mon CV
+				    </a>
+                        </div>
                     </div>
-                </div>
+                </section>
+                <div id="myCv" className="mb-3 text-center d-flex flex-column bg-secondary" >
 
-                <div className="text-center mt-4">
-                    <a id="show-cv" className="btn btn-xl btn-outline-light rounded js-scroll-trigger" href="#myCv">
-                        Voir mon CV</a>
+                    {this.state.showCv === true ?
+                        <>
+                            <Cv onCloseCv={this.handleCloseCv} />
+                            <a
+                                onClick={this.handleCloseCv} id="close-cv"
+                                className="btn btn-primary btn-xl rounded js-scroll-trigger mt-5 mb-5 mx-auto" href="#about">
+                                Fermer</a>
+                        </>
+                        : <></>}
+
                 </div>
-            </div>
-        </section>
-    )
+            </>
+        )
+    }
 }
---
+++ src/components/about/Cv.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Cv from '../../shared/img/CV.png';
+
+export default () => {
+    return <div className="pt-5"><img src={Cv} alt="Mon CV" className="mx-auto cv mt-5 pt-5" /></div>
+}
